4c5992c13220984878181b935934dfb21d033e43
AkaSan242 <Lucaka@icloud.com>
2022-05-05 10:15:09 +0200

    update of the two functions with assert to check the response data

--- test_unknow_email.py
+++ test_unknow_email.py
@@ -10,10 +10,12 @@ def test_should_fail_with_unknow_email(client):
     email = 'fake@test.com'
     response = client.post('/showSummary', data={'email': email})
     assert response.status_code == 500
+    assert "Sorry we cannot found" in response.text
     
 
 def test_should_pass_with_register_email(client):
     email = 'john@simplylift.co'
     response = client.post('/showSummary', data={'email': email})
     assert response.status_code == 200
+    assert "Welcome" in response.text
 
