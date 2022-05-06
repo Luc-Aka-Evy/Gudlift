90631b3f7e42ccd861d2f2f716bd09c1276b9726
AkaSan242 <Lucaka@icloud.com>
2022-05-06 12:35:04 +0200

    remove post request for login client

--- test_points_display.py
+++ test_points_display.py
@@ -3,8 +3,6 @@ from conftest import client
 
 
 def test_should_get_points_display_board_page(client):
-    email = 'john@simplylift.co'
-    client.post('/showSummary', data={'email': email})
     response = client.get('/pointsDisplay')
     assert response.status_code == 200
     assert "Points Display board" in response.text
