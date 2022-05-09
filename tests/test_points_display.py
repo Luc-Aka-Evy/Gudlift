dee17f482c1fb8c70627f670d858e678c932abcb
AkaSan242 <Lucaka@icloud.com>
2022-05-09 13:09:55 +0200

    unit test

---
+++ tests/test_points_display.py
@@ -0,0 +1,8 @@
+import pytest
+from .conftest import client
+
+
+def test_should_get_points_display_board_page(client):
+    response = client.get('/pointsDisplay')
+    assert response.status_code == 200
+    assert "Points Display board" in response.text
