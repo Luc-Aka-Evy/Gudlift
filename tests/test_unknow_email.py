5e974a93a56336d9287f5066a21680564eeafa43
AkaSan242 <Lucaka@icloud.com>
2022-05-09 12:26:48 +0200

    unit test

---
+++ tests/test_unknow_email.py
@@ -0,0 +1,21 @@
+from http import server
+from urllib import response
+import pytest
+
+
+from .conftest import client
+
+
+def test_should_fail_with_unknow_email(client):
+    email = 'fake@test.com'
+    response = client.post('/showSummary', data={'email': email})
+    assert response.status_code == 500
+    assert "Sorry we cannot found" in response.text
+    
+
+def test_should_pass_with_register_email(client):
+    email = 'john@simplylift.co'
+    response = client.post('/showSummary', data={'email': email})
+    assert response.status_code == 200
+    assert "Welcome" in response.text
+
