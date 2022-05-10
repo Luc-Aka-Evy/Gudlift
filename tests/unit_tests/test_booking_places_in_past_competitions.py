bd6e18ebb07fb7225431bb401a24317f10879ac9
AkaSan242 <Lucaka@icloud.com>
2022-05-10 10:49:44 +0200

    add test_logout.py

---
+++ tests/unit_tests/test_booking_places_in_past_competitions.py
@@ -0,0 +1,10 @@
+import pytest
+from .conftest import client
+
+
+def test_should_check_booking_places_in_past_competition(client):
+    email = 'john@simplylift.co'
+    response = client.post('/showSummary',data={'email': email})
+    assert response.status_code == 200
+    assert "This competition is full or over" in response.text
+    assert "Book Places" in response.text
